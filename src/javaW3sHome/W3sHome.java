22585f68db8743d6ac4ef577fb42a640cfeb714e
skypse <skypse0136@gmail.com>
2025-04-26 21:13:44 -0300

    commit w3schools ex01

---
+++ src/javaW3sHome/W3sHome.java
@@ -0,0 +1,13 @@
+package javaW3sHome;
+
+public class W3sHome {
+    public static void main(String[] args)
+    {
+        System.out.println("Olá mundo! (Hello World!)");
+        System.out.println("Estou aprendendo Java (I am learning Java)");
+        System.out.println("Não é legal! (It is awesome!)");
+
+        System.out.print("Testando print ");
+        System.out.print("na mesma linha");
+    }
+}
