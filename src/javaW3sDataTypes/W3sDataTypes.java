1b3098ede116e9b7dc205583d6274b89f301bb5d
skypse <skypse0136@gmail.com>
2025-04-27 00:07:37 -0300

    commit w3schools ex08

---
+++ src/javaW3sDataTypes/W3sDataTypes.java
@@ -0,0 +1,34 @@
+package javaW3sDataTypes;
+
+public class W3sDataTypes {
+    public static void main(String[] args) {
+        /*
+        * — > Java Data Types:
+        * Como explicado uma variável em java precisa ter um tipo definido.
+        * Exemplo abaixo:
+        int myNum = 5;               // Inteiro
+        float myFloatNum = 5.99f;    // Número flutuante
+        char myChar = 'A';           // Character
+        boolean myBoolean = true;    // Booleano
+        String myText = "Oi";        // Texto
+        *
+        * Os tipos de dados são divididos em 2 grupos:
+        * 1- Primitivos: incluindo byte, short, int, long, float, double, boolean e char.
+        * 2- Não primitivos: incluindo String, Object, Class e Array.
+        *
+        * Primitive Data Types
+        * Um dado primitivo especifica o tipo de uma variável e o tipo de valores que ela pode armazenar.
+        * There are eight primitive data types in Java:
+        * Data Type     Description
+        * byte 	        Stores whole numbers from -128 to 127
+        * short 	    Stores whole numbers from -32,768 to 32,767
+        * int 	        Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        * long 	        Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        * float 	    Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        * double 	    Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+        * boolean 	    Stores true or false values
+        * char 	        Stores a single character/letter or ASCII values
+        * */
+
+    }
+}
