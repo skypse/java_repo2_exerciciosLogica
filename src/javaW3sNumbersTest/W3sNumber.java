9cc1e6c75a88cbcea054c56d78a6c0f8fc703d48
skypse <skypse0136@gmail.com>
2025-04-26 21:28:05 -0300

    commit w3schools ex02

---
+++ src/javaW3sNumbersTest/W3sNumber.java
@@ -0,0 +1,13 @@
+package javaW3sNumbersTest;
+
+public class W3sNumber {
+    public static void main(String[]args)
+    {
+        // soma
+        System.out.println(3 + 3);
+        // multiplicacao
+        System.out.println(3 * 3);
+        // divisao
+        System.out.println(15 / 3);
+    }
+}
