3f4945934e9b63f4c67f7c205cdc265deefd5d3d
skypse <skypse0136@gmail.com>
2025-04-30 19:40:48 -0300

    commit ex23

---
+++ src/javaW3sIfElse/W3sIfElse03.java
@@ -0,0 +1,11 @@
+package javaW3sIfElse;
+
+public class W3sIfElse03 {
+    public static void main(String[] args) {
+        // short hand if
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+
+    }
+}
