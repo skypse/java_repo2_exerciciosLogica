cae56810e15299d6dac83dc2cd590293464bd64f
skypse <skypse0136@gmail.com>
2025-04-29 21:16:15 -0300

    commit ex13

---
+++ src/javaW3sOperators/W3sOperator01.java
@@ -0,0 +1,14 @@
+package javaW3sOperators;
+
+public class W3sOperator01 {
+    public static void main(String[] args) {
+        // +
+        int a = 10 + 5;
+        System.out.println(a);
+
+        int sum01 = 100 + 50;       // output = 150
+        int sum02 = sum01 + 100;    // output = 250
+        int sum03 = sum02 + sum02;  // output = 500
+        System.out.println(sum03);
+    }
+}
