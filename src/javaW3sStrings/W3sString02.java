f65eb309f7ee696abe3719a39ad0135516365a63
skypse <skypse0136@gmail.com>
2025-04-30 17:29:26 -0300

    commit ex19

---
+++ src/javaW3sStrings/W3sString02.java
@@ -0,0 +1,12 @@
+package javaW3sStrings;
+
+public class W3sString02 {
+    public static void main(String[] args) {
+        String txt1 = "We are the so-called \"Vikings\" from the north.";
+        String txt2 = "It's alright.";
+        String txt3 = "The character \\ is called backslash.";
+        System.out.println(txt1);
+        System.out.println(txt2);
+        System.out.println(txt3);
+    }
+}
