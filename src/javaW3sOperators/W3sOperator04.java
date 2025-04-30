c5551af44252249f6e82edca5e6ac54ec979f464
skypse <skypse0136@gmail.com>
2025-04-29 22:35:23 -0300

    commit ex16

---
+++ src/javaW3sOperators/W3sOperator04.java
@@ -0,0 +1,33 @@
+package javaW3sOperators;
+
+public class W3sOperator04 {
+    public static void main(String[] args) {
+        /*Java Comparison Operators
+        * São usados para comparar valores e retornar um resultado booleano.
+        * Exemplos:*/
+        // > (maior que)
+        int a = 10;
+        int b = 5;
+        System.out.println(a > b); // retorna true, porque o 10 é maior que o 5
+        // < (menor que)
+        int c = 10;
+        int d = 5;
+        System.out.println(c < d); // retorna false, porque 10 não é menor que 5
+        // == (igual á)
+        int e = 10;
+        int f = 5;
+        System.out.println(e == f); // retorna false, porque 10 não é igual a 5
+        // != (não igual)
+        int g = 10;
+        int h = 5;
+        System.out.println(g != h); // retorna true, porque 10 não é igual a 5
+        // >= (maior ou igual)
+        int i = 10;
+        int j = 5;
+        System.out.println(i >= j); // retorna true, porque 10 é maior que 5 ou igual a 5
+        // <= (menor ou igual)
+        int k = 10;
+        int l = 5;
+        System.out.println(k <= l); // retorna false, porque 10 não é menor que 5 ou igual a 5
+    }
+}
