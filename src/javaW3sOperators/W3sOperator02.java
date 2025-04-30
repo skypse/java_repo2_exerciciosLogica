b982ad1e0d3d9d1873e0b50c89fbec884d9c7aa1
skypse <skypse0136@gmail.com>
2025-04-29 21:17:50 -0300

    commit ex14

---
+++ src/javaW3sOperators/W3sOperator02.java
@@ -0,0 +1,53 @@
+package javaW3sOperators;
+
+public class W3sOperator02 {
+    public static void main(String[] args) {
+        /* Em java é divido os operadores em grupos:
+         * - Arithmetic operators
+         * - Assignment operators
+         * - Comparison operators
+         * - Logical operators
+         * - Bitwise operators
+         * */
+
+        // Addition
+        int x1 = 10;
+        int y1 = 5;
+        int z1 = x1 + y1;
+        System.out.println(z1);
+
+        // Subtraction
+        int x2 = 10;
+        int y2 = 5;
+        int z2 = x2 - y2;
+        System.out.println(z2);
+
+        // Multiplication
+        int x3 = 10;
+        int y3 = 5;
+        int z3 = x3 * y3;
+        System.out.println(z3);
+
+        // Division
+        int x4 = 10;
+        int y4 = 5;
+        int z4 = x4 / y4;
+        System.out.println(z4);
+
+        // Modulus
+        int x5 = 5;
+        int y5 = 2;
+        int z5 = x5 % y5;
+        System.out.println(z5);
+
+        // Increment
+        int x6 = 5;
+        ++x6;
+        System.out.println(x6);
+
+        // Decrement
+        int x7 = 5;
+        --x7;
+        System.out.println(x7);
+    }
+}
