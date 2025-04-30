5e177731188c60eebf7cd059e8000c395c028f59
skypse <skypse0136@gmail.com>
2025-04-30 20:03:44 -0300

    commit ex25

---
+++ src/javaW3sSwitchCase/W3sSwitchCase01.java
@@ -0,0 +1,44 @@
+package javaW3sSwitchCase;
+
+public class W3sSwitchCase01 {
+    public static void main(String[] args) {
+        /*Java Switch Statements:
+        * Invés de escrever muito if...else, você pode utilizar o 'switch'
+        * Exemplo:*/
+        int weekDay = 4;
+        switch (weekDay) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+        int day = 4;
+        switch (day) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+    }
+}
