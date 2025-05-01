7f177eec63bb705870640e48d2d7d6a54f6053e5
skypse <skypse0136@gmail.com>
2025-05-01 01:42:11 -0300

    commit ex30

---
+++ src/javaW3sFor/W3sForEach02.java
@@ -0,0 +1,18 @@
+package javaW3sFor;
+
+public class W3sForEach02 {
+    public static void main(String[] args) {
+        // Exemplo 01:
+        // Contando até 100!
+        for (int i = 1; i <= 100; i++)
+        {
+            System.out.println(i);
+        }
+
+        int number = 2;
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
